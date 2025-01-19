3ab8a2f2a14287fc18c41dfea940c8c310a43267
jadams102 <joeladamsdesign@gmail.com>
2025-01-19 11:24:58 -0800

    Firebase Setup - Update initial auth functions

--- src/app/lib/firebase/auth.ts
+++ src/app/lib/firebase/auth.ts
@@ -7,9 +7,20 @@ import {
     GoogleAuthProvider, 
     signInWithPopup 
   } from 'firebase/auth';
-  import { auth } from './firebase';
+  import { getDoc,doc, setDoc } from 'firebase/firestore';
+  import { auth, db } from './firebase';
   
-  export const signUp = (email: string, password: string) => createUserWithEmailAndPassword(auth, email, password);
+  export const signUp = async (email: string, password: string, fullName: string, role: 'admin' | 'editor') => {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    await setDoc(doc(db, 'users', user.uid), {
+      fullName,
+      role,
+      email: user.email,
+      uid: user.uid,
+    });
+    return user;
+  };
   
   export const signIn = (email: string, password: string) => signInWithEmailAndPassword(auth, email, password);
   
@@ -17,8 +28,22 @@ import {
   
   export const subscribeToAuthChanges = (callback: (user: User | null) => void) => onAuthStateChanged(auth, callback);
   
-  export const signInWithGoogle = async () => {
+  export const signInWithGoogle = async (role: 'admin' | 'editor') => {
     const provider = new GoogleAuthProvider();
-    return signInWithPopup(auth, provider);
+    const userCredential = await signInWithPopup(auth, provider);
+    const user = userCredential.user;
+    const userDocRef = doc(db, 'users', user.uid);
+    const userSnapshot = await getDoc(userDocRef);
+    const userExists = userSnapshot.exists();
+    
+    if (!userExists) {
+      await setDoc(userDocRef, {
+        fullName: user.displayName || 'Anonymous',
+        role,
+        email: user.email,
+        uid: user.uid,
+      });
+    }
+    return user;
   };
   
